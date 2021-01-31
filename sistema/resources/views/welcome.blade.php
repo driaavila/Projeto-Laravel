9c1dc05776d7a28cfdaaa4705a9c9ce4db2d7269
Adriana Ávila <avilaapadriana@gmail.com>
2021-01-31 19:00:33 -0300

    Quarta versão

--- sistema/resources/views/welcome.blade.php
+++ sistema/resources/views/welcome.blade.php
@@ -7,4 +7,28 @@
     <p>{{ $receitas->title }}</p>
     @endforeach
 
+    <div id="search-container" class="col-md-12">
+        <h1>Busque uma receita</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
+    <div id="receitas-container" class="col-md-3">
+        <h2>Outras Receitas</h2>
+        <p class="subtitle">Receitas em destaque </p>
+        <div id="cards-container" class="row">
+            @foreach($receitas as $receitas)
+            <div class="card-col-md-3">
+                <img src="/img/fazerreceita.jpg" width='200px'> 
+                <div class="card-body">
+                    <h5 class="card-title"></h5> 
+                    <p class="card-porcao">X porções</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+    </div>
+
 @endsection
